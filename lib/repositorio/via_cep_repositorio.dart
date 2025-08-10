b9a62684146fe2a51e20d15f7fcdee58cf493bbd
Wagner Soares <wagnersoares.redgaw@gmail.com>
2025-08-09 23:13:07 -0300

    atualização no repositorio

--- lib/repositorio/via_cep_repositorio.dart
+++ lib/repositorio/via_cep_repositorio.dart
@@ -1,32 +1,15 @@
-import 'dart:convert';
-
 import 'package:consulta_cep/modelo/b4a_cep_modelo.dart';
 import 'package:consulta_cep/modelo/via_cep_modelo.dart';
-import 'package:dio/dio.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart' as http;
+import 'package:consulta_cep/repositorio/custom_repositorio.dart';
 
 class ViaCepRepositorio {
-  var _dio = Dio();
-  ViaCepRepositorio() {
-    _dio = Dio();
-    _dio.options.headers["X-Parse-Application-Id"] = dotenv.get(
-      "XPARSEAPPLICATIONID",
-    );
-    _dio.options.headers["X-Parse-REST-API-Key"] = dotenv.get(
-      "XPERSERESTAPIKEY",
-    );
-    _dio.options.headers["Content-Type"] = "application/json";
-    _dio.options.baseUrl = dotenv.get("URLBASE");
-  }
+  final _dio = CustomRepositorio();
+  ViaCepRepositorio();
 
   Future<ViaCepModelo> cosultaCEP(String cep) async {
-    var resposta = await http.get(
-      Uri.parse("https://viacep.com.br/ws/$cep/json/"),
-    );
+    var resposta = await _dio.dio.get("https://viacep.com.br/ws/$cep/json/");
     if (resposta.statusCode == 200) {
-      var dado = jsonDecode(resposta.body);
-      return ViaCepModelo.fromJson(dado);
+      return ViaCepModelo.fromJson(resposta.data);
     }
     return ViaCepModelo();
   }
@@ -34,7 +17,7 @@ class ViaCepRepositorio {
   Future<B4aCepModelo> b4aListaCEP() async {
     var url = "/lista_cep";
 
-    var resposta = await _dio.get(url);
+    var resposta = await _dio.dio.get(url);
 
     if (resposta.statusCode == 200) {
       return B4aCepModelo.fromJson(resposta.data);
@@ -47,7 +30,7 @@ class ViaCepRepositorio {
     var url = "/lista_cep";
     url = "$url?where={\"cep\":\"$cep\"}";
 
-    var resposta = await _dio.get(url);
+    var resposta = await _dio.dio.get(url);
 
     if (resposta.statusCode == 200) {
       return B4aCepModelo.fromJson(resposta.data);
@@ -57,17 +40,17 @@ class ViaCepRepositorio {
   }
 
   Future<void> salver(CepB4aModelo cepB4aModelo) async {
-    await _dio.post("/lista_cep", data: cepB4aModelo.toJsonEndpoint());
+    await _dio.dio.post("/lista_cep", data: cepB4aModelo.toJsonEndpoint());
   }
 
   Future<void> atualizar(CepB4aModelo cepB4aModelo) async {
-    await _dio.put(
+    await _dio.dio.put(
       "/lista_cep/${cepB4aModelo.objectId}",
       data: cepB4aModelo.toJsonEndpoint(),
     );
   }
 
   Future<void> remover(String objectId) async {
-    await _dio.delete("/lista_cep/$objectId");
+    await _dio.dio.delete("/lista_cep/$objectId");
   }
 }
